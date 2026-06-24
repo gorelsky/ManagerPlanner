9255b69f1849207dec22e6100871373d10b61852
Евгений Горельский <e.gorelskiy@SLSP.local>
2026-06-24 16:25:36 +0300

    Configure Drizzle and server; add local env to gitignore

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -58,14 +59,11 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
+  // This serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
+  const port = parseInt(process.env.PORT || "5000", 10);
+
+  server.listen(port, "127.0.0.1", () => {
+    console.log(`serving on http://127.0.0.1:${port}`);
   });
 })();
